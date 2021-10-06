21677aff9350fb8d59f50820f1597b71465c91f0
NIKHILG2019 <52930019+NIKHILG2019@users.noreply.github.com>
2021-10-06 14:59:45 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tic_tac_toe_mcc_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tic_tac_toe_mcc_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac toe Game',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Tic Tac toe Game'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child:Text("Hello world")
+      ),
+    );
+  }
+}
