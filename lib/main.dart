2622175b4babebb51f57ca18befb05ed6601bac3
NIKHILG2019 <52930019+NIKHILG2019@users.noreply.github.com>
2021-10-06 17:19:06 +0530

    Homepage Update 1. Setting the Title in the AppBar also keeping it in center position. 2. Setting the App color theme to brown 3. Changing the background color to light brown 4. Added Gif , instruction and Button to the Homepage. 5. Alignment of all the widgets in the HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tic Tac toe Game',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      home: MyHomePage(title: 'Tic Tac toe Game'),
+      home: MyHomePage(title: 'Tic Tac Toe'),
     );
   }
 }
@@ -29,15 +30,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-
+    final ButtonStyle startButton = ElevatedButton.styleFrom(elevation: 5,minimumSize: Size(100, 50));
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: Center(
-        child:Text("Hello world")
-      ),
-    );
+      body: Container(
+        color: Colors.brown[100],
+          child:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Image.asset('assets/tic_tac_toe.gif'),
+              Container(
+                padding: EdgeInsets.only(top:40),
+              child:Text("Instructions",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold ))),
+              Container(
+                padding: EdgeInsets.only(bottom: 50,top: 20),
+                  child:Center(child:Column(
+                children: [
+                  Text("1. This game is a two player game",style: TextStyle(fontSize: 16),),
+                  Text("2. Press the Start button to start the game",style: TextStyle(fontSize: 16)),
+        ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ))
+        ),
+        ElevatedButton(
+          style: startButton,
+          onPressed: (){},
+          child: Container(
+            height: 50,
+              width: 100,
+              child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[Text("Start",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),Icon(Icons.arrow_right_outlined)])),
+        ),
+    ]))
+    ));
   }
 }
