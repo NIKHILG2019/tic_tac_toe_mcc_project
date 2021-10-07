4f8697ad1246c382a6ca13221dc6ebd25209a6c6
NIKHILG2019 <52930019+NIKHILG2019@users.noreply.github.com>
2021-10-07 09:15:24 +0530

    Added Game matrixComputations to decide who is the winner in the game. Also to make it look resposive colorMatrix is also created After winner is declated other all unselected buttons are disabled.

--- lib/GamePage.dart
+++ lib/GamePage.dart
@@ -18,6 +18,7 @@ class _GamePageState extends State<GamePage> {
   int winner = -1;
   bool xAndOFlip = false;
   List<List<int>> matrix = List.generate(3, (index) => List<int>.filled(3, -1),growable: false);
+  List<List<int>> colorMatrix = List.generate(3, (index) => List<int>.filled(3, -1),growable: false);
   final ButtonStyle startButton =
       ElevatedButton.styleFrom(elevation: 5, minimumSize: Size(100, 50));
 
@@ -37,6 +38,166 @@ class _GamePageState extends State<GamePage> {
      setState(() {
        xAndOFlip = !xAndOFlip;
      });
+    }
+    if(matrix[0][0] == 1 && matrix[0][1] == 1 && matrix[0][2]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[0][1] = 2;
+        colorMatrix[0][2]=2;
+      });
+    }
+    else if(matrix[1][0] == 1 && matrix[1][1] == 1 && matrix[1][2]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[1][0] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[1][2] = 2;
+      });
+    }
+    else if(matrix[2][0] == 1 && matrix[2][1] == 1 && matrix[2][2]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[2][0] = 2;
+        colorMatrix[2][1] = 2;
+        colorMatrix[2][2]=2;
+      });
+    }
+    else if(matrix[0][0] == 1 && matrix[1][0] == 1 && matrix[2][0]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[1][0] = 2;
+        colorMatrix[2][0] = 2;
+      });
+    }
+    else if(matrix[0][1] == 1 && matrix[1][1] == 1 && matrix[2][1]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][1] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][1] = 2;
+      });
+    }
+    else if(matrix[0][2] == 1 && matrix[1][2] == 1 && matrix[2][2]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][2] = 2;
+        colorMatrix[1][2] = 2;
+        colorMatrix[2][2] = 2;
+      });
+    }
+    else if(matrix[0][0] == 1 && matrix[1][1] == 1 && matrix[2][2]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][2] = 2;
+      });
+    }
+    else if(matrix[0][2] == 1 && matrix[1][1] == 1 && matrix[2][0]==1){
+      setState(() {
+        winner=1;
+      });
+      setState(() {
+        colorMatrix[0][2] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][0] = 2;
+      });
+    }
+    else if(matrix[0][0] == 0 && matrix[0][1] == 0 && matrix[0][2]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[0][1] = 2;
+        colorMatrix[0][2] = 2;
+      });
+    }
+    else if(matrix[1][0] == 0 && matrix[1][1] == 0 && matrix[1][2]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[1][0] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[1][2] = 2;
+      });
+    }
+    else if(matrix[2][0] == 0 && matrix[2][1] == 0 && matrix[2][2]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[2][0] = 2;
+        colorMatrix[2][1] = 2;
+        colorMatrix[2][2] = 2;
+      });
+    }
+    else if(matrix[0][0] == 0 && matrix[1][0] == 0 && matrix[2][0]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[1][0] = 2;
+        colorMatrix[2][0] = 2;
+      });
+    }
+    else if(matrix[0][1] == 0 && matrix[1][1] == 0 && matrix[2][1]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][1] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][1] = 2;
+      });
+    }
+    else if(matrix[0][2] == 0 && matrix[1][2] == 0 && matrix[2][2]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][2] = 2;
+        colorMatrix[1][2] = 2;
+        colorMatrix[2][2] = 2;
+      });
+    }
+    else if(matrix[0][0] == 0 && matrix[1][1] == 0 && matrix[2][2]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][0] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][2] = 2;
+      });
+    }
+    else if(matrix[0][2] == 0 && matrix[1][1] == 0 && matrix[2][0]==0){
+      setState(() {
+        winner=0;
+      });
+      setState(() {
+        colorMatrix[0][2] = 2;
+        colorMatrix[1][1] = 2;
+        colorMatrix[2][0] = 2;
+      });
     }
 
   }
@@ -75,12 +236,13 @@ class _GamePageState extends State<GamePage> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            matrix[0][0] == -1 ? GameButton(0,0,matrixComputation) :
+                            matrix[0][0] == -1 && winner == -1 ? GameButton(0,0,matrixComputation) :
                             Container(
+                              color: colorMatrix[0][0] == 2?Colors.lightGreen:Colors.brown[200],
                               alignment: Alignment.center,
                               width: 100,
                               height: 100,
-                              child:Text(matrix[0][0] == 0 ? "O":"X",
+                              child:Text(matrix[0][0] == 0 ? "O":matrix[0][0]==1?"X":"",
                               style: TextStyle(fontSize: 50, color: Colors.black)))
                             ,
                             Container(
@@ -90,12 +252,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[0][1] == -1 ? GameButton(0,1,matrixComputation) :
+                            matrix[0][1] == -1  && winner == -1 ? GameButton(0,1,matrixComputation) :
                             Container(
+                                color: colorMatrix[0][1] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[0][1] == 0 ? "O":"X",
+                                child:Text(matrix[0][1] == 0 ? "O":matrix[0][1]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                             Container(
                               width: 5,
@@ -104,12 +267,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[0][2] == -1 ? GameButton(0,2,matrixComputation) :
+                            matrix[0][2] == -1  && winner == -1 ? GameButton(0,2,matrixComputation) :
                             Container(
+                                color: colorMatrix[0][2] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[0][2] == 0 ? "O":"X",
+                                child:Text(matrix[0][2] == 0 ? "O":matrix[0][2]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black)))
                           ],
                         ),
@@ -119,12 +283,13 @@ class _GamePageState extends State<GamePage> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            matrix[1][0] == -1 ? GameButton(1,0,matrixComputation) :
+                            matrix[1][0] == -1  && winner == -1 ? GameButton(1,0,matrixComputation) :
                             Container(
+                                color: colorMatrix[1][0] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[1][0] == 0 ? "O":"X",
+                                child:Text(matrix[1][0] == 0 ? "O":matrix[1][0]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                             Container(
                               width: 5,
@@ -133,12 +298,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[1][1] == -1 ? GameButton(1,1,matrixComputation) :
+                            matrix[1][1] == -1  && winner == -1 ? GameButton(1,1,matrixComputation) :
                             Container(
+                                color: colorMatrix[1][1] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[1][1] == 0 ? "O":"X",
+                                child:Text(matrix[1][1] == 0 ? "O":matrix[1][1]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                             Container(
                               width: 5,
@@ -147,12 +313,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[1][2] == -1 ? GameButton(1,2,matrixComputation) :
+                            matrix[1][2] == -1  && winner == -1 ? GameButton(1,2,matrixComputation) :
                             Container(
+                                color: colorMatrix[1][2] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[1][2] == 0 ? "O":"X",
+                                child:Text(matrix[1][2] == 0 ? "O":matrix[1][2]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black)))
                           ],
                         ),
@@ -162,12 +329,13 @@ class _GamePageState extends State<GamePage> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            matrix[2][0] == -1 ? GameButton(2,0,matrixComputation) :
+                            matrix[2][0] == -1  && winner == -1 ? GameButton(2,0,matrixComputation) :
                             Container(
+                                color: colorMatrix[2][0] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[2][0] == 0 ? "O":"X",
+                                child:Text(matrix[2][0] == 0 ? "O":matrix[2][0]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                             Container(
                               width: 5,
@@ -176,12 +344,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[2][1] == -1 ? GameButton(2,1,matrixComputation) :
+                            matrix[2][1] == -1  && winner == -1 ? GameButton(2,1,matrixComputation) :
                             Container(
+                                color: colorMatrix[2][1] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[2][1] == 0 ? "O":"X",
+                                child:Text(matrix[2][1] == 0 ? "O": matrix[2][1]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                             Container(
                               width: 5,
@@ -190,12 +359,13 @@ class _GamePageState extends State<GamePage> {
                               margin: EdgeInsets.all(2),
                               padding: EdgeInsets.all(0),
                             ),
-                            matrix[2][2] == -1 ? GameButton(2,2,matrixComputation) :
+                            matrix[2][2] == -1  && winner == -1 ? GameButton(2,2,matrixComputation) :
                             Container(
+                                color: colorMatrix[2][2] == 2?Colors.lightGreen:Colors.brown[200],
                                 alignment: Alignment.center,
                                 width: 100,
                                 height: 100,
-                                child:Text(matrix[2][2] == 0 ? "O":"X",
+                                child:Text(matrix[2][2] == 0 ? "O": matrix[2][2]==1?"X":"",
                                     style: TextStyle(fontSize: 50, color: Colors.black))),
                           ],
                         )
@@ -250,7 +420,3 @@ class _GamePageState extends State<GamePage> {
   }
 }
 
-// Container(
-// child: Center(
-// child: Text(widget.playerTwoName),
-// ))
